5a23b191c44b8ff6d4d63d26d11179509c8e3f06
Egi2018 <kovacsegon806@gmail.com>
2021-11-23 20:02:26 +0100

    2 jatekos mode done

--- src/main/java/malom/model/allapot/JatekosLevesz.java
+++ src/main/java/malom/model/allapot/JatekosLevesz.java
@@ -20,42 +20,36 @@ public class JatekosLevesz extends JatekosAllapot {
 
     @Override
     public void vegrehajt(Pozicio pozicio) {
-        if(levehetE(pozicio))
+        if (levehetE(pozicio))
             palya.lehelyezJatekElem(pozicio, new Ures());
-        if(nyert() && palya.getMasikJatekos().getKorSzamlalo() >= MAX_KORSZAM) palya.jatekVege();
+        if (nyert() && palya.getMasikJatekos().getKorSzamlalo() >= MAX_KORSZAM) palya.jatekVege();
     }
 
     @Override
     public void setKovetkezoAllapot(Pozicio pozicio) {
-        if(levehetE(pozicio)) {
-            if(palya.getJatekos().getKorSzamlalo() >= MAX_KORSZAM){
-                this.palya.setJatekosAllapot(new JatekosKijelol(palya));
-            } else{
-                this.palya.setJatekosAllapot(new JatekosLerak(palya));
-            }
-            palya.valtJatekos();
+        if (!levehetE(pozicio)) return;
+        if (palya.getJatekos().getKorSzamlalo() >= MAX_KORSZAM) {
+            this.palya.setJatekosAllapot(new JatekosKijelol(palya));
+        } else {
+            this.palya.setJatekosAllapot(new JatekosLerak(palya));
         }
+        palya.valtJatekos();
     }
 
     private boolean levehetE(Pozicio pozicio) {
-        return palya.malomE(pozicio) && getKorongSzam() == 3 && palya.getMasikJatekos().getKorSzamlalo() >= MAX_KORSZAM
+        return (palya.malomE(pozicio) && getKorongSzam() == 3
+                && palya.getMasikJatekos().getKorSzamlalo() >= MAX_KORSZAM)
                 || !palya.malomE(pozicio);
     }
 
     public boolean nyert() {
-        int korongSzamlalo = 0;
-        for (int i = 0; i < palya.getJatekElemek().length; i++) {
-            for (int j = 0; j < palya.getJatekElemek()[0].length; j++){
-                if (palya.masikJatekosSzinEgyezikMezonLevoKoronggal(of(i, j))) korongSzamlalo++;
-            }
-        }
-        return korongSzamlalo < 3;
+        return getKorongSzam() < 3;
     }
 
-    private int getKorongSzam(){
-        int korongSzam  = 0;
+    private int getKorongSzam() {
+        int korongSzam = 0;
         for (int i = 0; i < palya.getJatekElemek().length; i++) {
-            for (int j = 0; j < palya.getJatekElemek()[0].length; j++){
+            for (int j = 0; j < palya.getJatekElemek()[0].length; j++) {
                 if (palya.masikJatekosSzinEgyezikMezonLevoKoronggal(of(i, j))) korongSzam++;
             }
         }
